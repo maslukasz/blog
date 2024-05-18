1d8ba1b9be60051d57379f847a9438c0b2d1aa4e
maslukasz <lukesekmc@gmail.com>
2024-05-18 23:30:44 +0200

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <li class="posts">
+            <h1>ephemeris</h1>
+            <ul>⛺ <strong>15.05.2024</strong> | <a href='#'>Namiot serca</a></ul>
+            <ul>📖 <strong>10.05.2024</strong> | <a href="#">Odmawiajcie psalmy</a></ul>
+        </li>
+    </div>
+</body>
+
+</html>
